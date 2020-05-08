67e1525b6d9dd8756d952fea1bdcbc47c4bc7913
Anuschka Faucci <anufaucci@gmail.com>
2020-05-07 18:43:41 -1000

    Initial Commit

---
+++ HackwichTwo-v2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HackwichTwo-v2
+//
+//  Created by Anuschka Faucci on 5/7/20.
+//  Copyright © 2020 Anuschka Faucci. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ HackwichTwo-v2Tests/HackwichTwo_v2Tests.swift
@@ -0,0 +1,34 @@
+//
+//  HackwichTwo_v2Tests.swift
+//  HackwichTwo-v2Tests
+//
+//  Created by Anuschka Faucci on 5/7/20.
+//  Copyright © 2020 Anuschka Faucci. All rights reserved.
+//
+
+import XCTest
+@testable import HackwichTwo_v2
+
+class HackwichTwo_v2Tests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
